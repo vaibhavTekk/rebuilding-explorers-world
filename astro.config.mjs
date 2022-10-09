73ff51a2f8d94cee3bc56e082f1865120c2b75db
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-10-09 21:20:41 +0530

    Addition of React and Vue
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import image from "@astrojs/image";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap(), tailwind(), prefetch(), react(), image(), vue()]
+}); // yarn astro add tailwind
---
+++ src/components/typescript/react/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function DoK() {
+  return (
+    <>
+      <section className="font-bold text-xl font-xl">
+        Welcome to the data on Kubernetes community
+      </section>
+    </>
+  );
+}
